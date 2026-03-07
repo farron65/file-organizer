2ed5f6eef05269e3d9cbea4d04fbbdf2cdd38431
farron65 <bezhansn@gmail.com>
2026-03-07 14:39:45 -0500

    Refactor: make FileMove class to handle file moving and retry logic, separation of logic

--- main.py
+++ main.py
@@ -13,45 +13,54 @@ from watchdog.observers import Observer
 
 logging.basicConfig(level=logging.DEBUG)
 
-class MyEventHandler(events.FileSystemEventHandler):
+class FileMove():
+    def __init__(self, file, dt):
+        self.file = file
+        self.dt = dt
+        
+    def move_file(self):
+        shutil.move(self.file, self.dt)
+        
+    def retry_move(self):
+        t = 0
+        while True:
+            if t > 100:
+                raise TimeoutError
+            t += 1
+            try:
+                self.move_file()
+                logging.info(f"Successfully moved the file '{Path(self.file).name}' to: '{self.dt}'")
+                break
+            except (PermissionError, FileNotFoundError):
+                pass          
+            logging.info("waiting")
+            time.sleep(5)
+
+class FileWatcher(events.FileSystemEventHandler):
     
     def __init__(self):
         super().__init__()
         with open("config.json", "r") as f:
-            self.classes = json.load(f)
+            self.lesson_path = json.load(f)
     
     def catch_handler(self, event: events.FileSystemEvent):
         file_path = str(event.src_path)
-        file: str = Path(file_path).name
-        tokens = re.split("[ -.]", file)
+        tokens = re.split("[ -.]", Path(file_path).name)
         if Path(file_path).suffix == ".tmp" or Path(file_path).suffix == ".crdownload":
             return
         for token in tokens:
-            cls = self.classes.get(token.lower())
+            cls = self.lesson_path.get(token.lower())
             if cls:
-                logging.debug(f"Found lesson {file} with a path: {cls}")
-                t = 0
-                while True:
-                    if t > 100:
-                        raise TimeoutError
-                    t += 1
-                    try:
-                        new_dt = f"{cls}/{file}"
-                        shutil.move(file_path, f"{new_dt}")
-                        logging.info(f"Successfully moved the file {file} to {new_dt}")
-                        break
-                    except (PermissionError, FileNotFoundError):
-                        pass          
-                    logging.info("waiting")
-                    time.sleep(5)
+                file_move = FileMove(file_path, cls)
+                file_move.retry_move()
     
     def on_moved(self, event: events.DirMovedEvent | events.FileMovedEvent) -> None:
         self.catch_handler(event)
 
     def on_created(self, event: events.DirCreatedEvent | events.FileCreatedEvent) -> None:
         self.catch_handler(event)
-
-event_handler = MyEventHandler()
+            
+event_handler = FileWatcher()
 
 observer = Observer()
 observer.schedule(event_handler, path="C:/Users/admin/Downloads", recursive=True)
